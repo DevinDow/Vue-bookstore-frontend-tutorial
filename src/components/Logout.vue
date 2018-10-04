12d18a71281c2a6e9898c69888ed7a359a7caeeb
Devin Dow <DevinDow@gmail.com>
2018-10-03 17:05:14 -0700

    add Logout

---
+++ src/components/Logout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div/>
+</template>
+
+<script>
+export default {
+  name: 'Logout',
+  created () {
+    console.log('Logout.created')
+    delete localStorage.token
+    this.$store.dispatch('logout')
+    this.$router.push('/')
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
+import Logout from '@/components/Logout'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history', // gets rid of '#' in URLs
   routes: [
     {
       path: '/',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/logout',
+      name: 'Logout',
+      component: Logout
     },
     {
       path: '/authors',
